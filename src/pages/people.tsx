ca57d50bc01a9d018261b355f79e58f83efcc134
Ekta_Gupta <108565384+01Ekta@users.noreply.github.com>
2024-07-04 00:12:11 +0530

    Create people.tsx

---
+++ src/pages/people.tsx
@@ -0,0 +1,11 @@
+// src/pages/people.tsx
+const People = () => {
+    return (
+      <div>
+        <h1>People</h1>
+        <p>Manage your contacts here.</p>
+      </div>
+    );
+  };
+  
+  export default People;
